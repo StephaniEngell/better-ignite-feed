f8d7496c3fbe14ed068b7a8a6de25e86a7e16a07
StephaniEngell <stephaniengel18@gmail.com>
2025-03-24 00:19:10 -0300

    ajust layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Stephani Engel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consequuntur quisquam dicta, officiis reiciendis, tempora magnam at laboriosam autem nulla error fugit exercitationem ducimus rem. Amet exercitationem neque veniam sequi."
-      />
 
-      <Post
-        author="Graziele Nanni"
-        content="Lorem ipsum dolor sit amet consectetur."
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author="Stephani Engel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consequuntur quisquam dicta, officiis reiciendis, tempora magnam at laboriosam autem nulla error fugit exercitationem ducimus rem. Amet exercitationem neque veniam sequi."
+          />
+
+          <Post
+            author="Graziele Nanni"
+            content="Lorem ipsum dolor sit amet consectetur."
+          />
+        </main>
+      </div>
     </>
   );
 }
