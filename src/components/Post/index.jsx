3b5c7fa6c51c896c5ffbef26de8d8c437ac41ecb
StephaniEngell <stephaniengel18@gmail.com>
2025-03-26 01:56:05 -0300

    Initialize Post component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -9,10 +9,30 @@ export function Post() {
             className={styles.avatar}
             src="https://avatars.githubusercontent.com/u/111245491?v=4"
           />
+          <div className={styles.authorInfo}>
+            <strong>Stephani Engel</strong>
+            <span>Software Engineer</span>
+          </div>
         </div>
 
-        <div className={styles.authorInfo}></div>
+        <time title="26 de março às 01:34h" datetime="2025-03-26 01:34:23">
+          Publicado há 1h
+        </time>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
     </article>
   );
 }
