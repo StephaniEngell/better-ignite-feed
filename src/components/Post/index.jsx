5bfb8e6ba5ef21c6b0022ed8c971257e587c4fec
StephaniEngell <stephaniengel18@gmail.com>
2025-03-27 00:31:01 -0300

    Prepare post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -30,9 +30,19 @@ export function Post() {
           <a href="#">👉 jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="#">#novoprojeto #nlw #rocketseat</a>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#nlw </a>
+          <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <button type="submit">Comentar</button>
+      </form>
     </article>
   );
 }
