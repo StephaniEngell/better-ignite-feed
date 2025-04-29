111dfc17ffc28d311e46e55cfe41fd4e24381f2f
StephaniEngell <stephaniengel18@gmail.com>
2025-04-28 22:21:17 -0300

    implement post interation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,52 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Stephani Engel",
+      role: "Software Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-03-26 01:34:23"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/92805039?v=4",
+      name: "Carlos Silva",
+      role: "Software Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-04-26 01:34:23"),
+  },
+];
+
 function App() {
   return (
     <>
@@ -13,15 +59,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Stephani Engel"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consequuntur quisquam dicta, officiis reiciendis, tempora magnam at laboriosam autem nulla error fugit exercitationem ducimus rem. Amet exercitationem neque veniam sequi."
-          />
-
-          <Post
-            author="Graziele Nanni"
-            content="Lorem ipsum dolor sit amet consectetur."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
